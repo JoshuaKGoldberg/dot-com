b24fe5d2d3ca38a8fa20ce71e89a1fecbf1240ff
Josh Goldberg <git@joshuakgoldberg.com>
2023-04-06 13:07:05 -0400

    Add lint-staged and husky

--- remark-reading-time.mjs
+++ remark-reading-time.mjs
@@ -1,5 +1,5 @@
-import getReadingTime from "reading-time";
 import { toString } from "mdast-util-to-string";
+import getReadingTime from "reading-time";
 
 export function remarkReadingTime() {
   return function (tree, { data }) {
