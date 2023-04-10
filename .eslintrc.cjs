15509d3c3177526fe0fb0fa4e2029903ac4a6874
Josh Goldberg <git@joshuakgoldberg.com>
2023-04-10 14:26:45 -0400

    [Reference] Trimmed down ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,8 +4,6 @@ module.exports = {
 		"plugin:@typescript-eslint/recommended",
 		"plugin:astro/recommended",
 		"plugin:jsx-a11y/recommended",
-		"plugin:regexp/recommended",
-		"plugin:typescript-sort-keys/recommended",
 	],
 	overrides: [
 		{
@@ -22,7 +20,6 @@ module.exports = {
 			rules: {
 				// TODO: Investigate?? :(
 				"@typescript-eslint/no-unsafe-assignment": "off",
-				"deprecation/deprecation": "off",
 			},
 		},
 		{
@@ -33,44 +30,8 @@ module.exports = {
 			],
 		},
 		{
-			files: ["*.json", "*.jsonc"],
-			excludedFiles: ["package.json"],
-			parser: "jsonc-eslint-parser",
-			rules: {
-				"jsonc/sort-keys": "error",
-			},
-			extends: ["plugin:jsonc/recommended-with-json"],
-		},
-		{
-			extends: ["plugin:markdown/recommended"],
-			files: ["**/*.md"],
-			processor: "markdown/markdown",
-		},
-		{
 			extends: ["plugin:solid/typescript"],
 			files: ["*.tsx"],
-		},
-		{
-			files: ["**/*.{yml,yaml}"],
-			parser: "yaml-eslint-parser",
-			extends: ["plugin:yml/standard", "plugin:yml/prettier"],
-			rules: {
-				"yml/file-extension": ["error", { extension: "yml" }],
-				"yml/sort-keys": [
-					"error",
-					{
-						order: { type: "asc" },
-						pathPattern: "^.*$",
-					},
-				],
-				"yml/sort-sequence-values": [
-					"error",
-					{
-						order: { type: "asc" },
-						pathPattern: "^.*$",
-					},
-				],
-			},
 		},
 	],
 	parser: "@typescript-eslint/parser",
@@ -78,27 +39,6 @@ module.exports = {
 		project: true,
 		tsconfigRootDir: __dirname,
 	},
-	plugins: [
-		"@typescript-eslint",
-		"astro",
-		"deprecation",
-		"jsx-a11y",
-		"regexp",
-		"solid",
-		"simple-import-sort",
-		"typescript-sort-keys",
-	],
+	plugins: ["@typescript-eslint", "astro", "jsx-a11y", "solid"],
 	root: true,
-	rules: {
-		"simple-import-sort/imports": "error",
-		"simple-import-sort/exports": "error",
-		"deprecation/deprecation": "error",
-
-		// Stylistic concerns that don't interfere with Prettier
-		"padding-line-between-statements": "off",
-		"@typescript-eslint/padding-line-between-statements": [
-			"error",
-			{ blankLine: "always", next: "*", prev: "block-like" },
-		],
-	},
 };
