b54832e7cac16a17b980e784dddb9e0331dfba61
Josh Goldberg <git@joshuakgoldberg.com>
2023-04-14 00:04:25 -0400

    Generally working Squiggly

--- src/components/Squiggly.tsx
+++ src/components/Squiggly.tsx
@@ -16,16 +16,19 @@ export type SquigglyProps<As extends keyof JSX.HTMLElementTags> = Omit<
 export function Squiggly<As extends keyof JSX.HTMLElementTags>(
 	props: SquigglyProps<As>
 ) {
-	const [knownProps, textProps] = splitProps(props, ["info", "title"]);
+	const [knownProps, textProps] = splitProps(props, ["class", "info", "title"]);
 
 	const idFromInfo = knownProps.info.replace(/[\W_]/g, "-").slice(0, 15);
-	const idFromTitle = knownProps.title.replace(/[\W_]/g, "-").slice(0, 15);
 	const [getTriggered, setTriggered] = createSignal(false);
 
 	return (
 		<div
 			aria-describedby={idFromInfo}
-			class={clsx([styles.squiggly, getTriggered() && styles.triggered])}
+			class={clsx([
+				styles.squiggly,
+				getTriggered() && styles.triggered,
+				knownProps.class,
+			])}
 			onBlur={() => setTriggered(false)}
 			onFocus={() => setTriggered(true)}
 			onKeyDown={(event) => {
@@ -35,14 +38,10 @@ export function Squiggly<As extends keyof JSX.HTMLElementTags>(
 			}}
 		>
 			{/* @ts-expect-error - Dynamic components in TypeScript are tough. */}
-			<Text for={idFromTitle} {...textProps} class={styles.label} as="label">
-				{knownProps.title}
-			</Text>
-			{/* @ts-expect-error - Dynamic components in TypeScript are tough. */}
 			<Text
 				{...textProps}
+				as="div"
 				class={styles.title}
-				id={idFromTitle}
 				tabIndex={0}
 				onFocus={() => setTriggered(true)}
 				onBlur={() => setTriggered(false)}
