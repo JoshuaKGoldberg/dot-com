7ae0ab3bd8a2a5c54b8055b273f1f129ff8e9db3
Josh Goldberg <git@joshuakgoldberg.com>
2023-07-24 17:38:56 -0500

    Reverse sort Upcoming speakings

--- src/components/SpeakingsGrouped.tsx
+++ src/components/SpeakingsGrouped.tsx
@@ -1,26 +1,36 @@
 import type { CollectionEntry } from "astro:content";
+import { For } from "solid-js";
 
 import { groupBy } from "../utils";
+import { EntryList } from "./EntryList";
 import { GroupedEntries } from "./GroupedEntries";
 import { SpeakingEntry } from "./SpeakingEntry";
 
 export interface SpeakingsGroupedProps {
-	speakings: CollectionEntry<"speaking">[];
+	previous: CollectionEntry<"speaking">[];
+	upcoming: CollectionEntry<"speaking">[];
 }
 
 export function SpeakingsGrouped(props: SpeakingsGroupedProps) {
 	return (
-		<GroupedEntries
-			groups={Object.entries(
-				groupBy(
-					props.speakings
-						// TODO: when I add in tabs, then I'll add in non-talks
-						.filter((speaking) => speaking.data.category === "Talks"),
-					(speaking) => yearOrUpcoming(speaking.data.date)
-				)
-			).sort(([a], [b]) => (a === "Upcoming" ? -1 : +b - +a))}
-			renderEntry={(speaking) => <SpeakingEntry speaking={speaking} />}
-		/>
+		<>
+			<EntryList category="Upcoming">
+				<For each={props.upcoming}>
+					{(speaking) => <SpeakingEntry speaking={speaking} />}
+				</For>
+			</EntryList>
+			<GroupedEntries
+				groups={Object.entries(
+					groupBy(
+						props.previous
+							// TODO: when I add in tabs, then I'll add in non-talks
+							.filter((speaking) => speaking.data.category === "Talks"),
+						(speaking) => yearOrUpcoming(speaking.data.date)
+					)
+				).sort(([a], [b]) => (a === "Upcoming" ? -1 : +b - +a))}
+				renderEntry={(speaking) => <SpeakingEntry speaking={speaking} />}
+			/>
+		</>
 	);
 }
 
