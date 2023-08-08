1ef391b3f42f0f5ba3935da3457eed62833a4260
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2023-08-08 10:23:32 -0700

    fix: touch up RSS links (#54)

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -8,11 +8,16 @@ export async function get(context: APIContext) {
 		customData: `<language>en-us</language>`,
 		description: blogDescription,
 		site: context.site?.toString() ?? site,
-		items: await pagesGlobToRssItems(
-			// TODO: find or file an issue?
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-call
-			import.meta.glob("../content/blog/**/*.mdx")
-		),
+		items: (
+			await pagesGlobToRssItems(
+				// TODO: find or file an issue?
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-call
+				import.meta.glob("../content/blog/**/*.mdx")
+			)
+		).map((inner) => ({
+			...inner,
+			link: inner.link.replace(/^src\/content/, "").replace(/index.mdx$/, ""),
+		})),
 		title: "Goldblog",
 	});
 }
