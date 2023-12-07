126d7a4a03d0367f7cbee507655f3eb9de38b028
Josh Goldberg <git@joshuakgoldberg.com>
2023-12-07 10:39:24 -0500

    built-in prefetch and pnpm dedupe

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,4 @@
 import mdx from "@astrojs/mdx";
-import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
 import { defineConfig, sharpImageService } from "astro/config";
@@ -12,9 +11,10 @@ export default defineConfig({
 	image: {
 		service: sharpImageService(),
 	},
-	integrations: [konamimojisplosion(), solidJs(), prefetch(), mdx(), sitemap()],
+	integrations: [konamimojisplosion(), solidJs(), mdx(), sitemap()],
 	markdown: {
 		syntaxHighlight: "prism",
 	},
+	prefetch: true,
 	site,
 });
