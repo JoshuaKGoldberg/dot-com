754ae5664d908d1e70c99d67f6fbc0d06e2f71ac
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2024-02-27 18:31:30 -0500

    chore: bump astro dependencies to latest and add astro check (#237)
    * chore: bump astro dependencies to latest
    
    * fix: rss issue
    
    * config.ts too I suppose
    
    * chore: fix lint and package dedupe
    
    * chore: meh, add check too, why not
    
    * chore: oops, check, not build
    
    * chore: bump accessible-astro-components too

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,21 +1,22 @@
 import type { APIContext } from "astro";
 
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 import { blogDescription, site } from "../constants";
 
 export async function GET(context: APIContext) {
+	const blog = await getCollection("blog");
+
 	return rss({
 		customData: `<language>en-us</language>`,
 		description: blogDescription,
-		items: (
-			await pagesGlobToRssItems(import.meta.glob("../content/blog/**/*.mdx"))
-		).map((inner) => ({
-			...inner,
-			link: inner.link
-				.replace(/^src\/content/, "")
-				.replace(/index.mdx$/, "")
-				.replaceAll(`'`, ""),
+		items: blog.map((post) => ({
+			customData: post.data.customData,
+			description: post.data.description,
+			link: `/blog/${post.slug}`,
+			pubDate: post.data.pubDate,
+			title: post.data.title,
 		})),
 		site: context.site?.toString() ?? site,
 		title: "Goldblog",
