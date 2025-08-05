cc1f5fa87e217587874ca042abbaa6db414316b8
Josh Goldberg <git@joshuakgoldberg.com>
2025-08-05 13:54:44 -0700

    pnpm format --write

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,7 @@
 // https://github.com/eslint-community/eslint-plugin-eslint-comments/issues/214
-declare module '@eslint-community/eslint-plugin-eslint-comments/configs' {
-  import type { Linter } from 'eslint';
+declare module "@eslint-community/eslint-plugin-eslint-comments/configs" {
+	import type { Linter } from "eslint";
 
-  const recommended: Linter.Config;
-  export = { recommended };
+	const recommended: Linter.Config;
+	export = { recommended };
 }
